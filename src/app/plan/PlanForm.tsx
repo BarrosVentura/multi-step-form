d652fad1998ad92a23165d74cc2a0a0e469d39b1
Adson Ventura <adsonventura@gmail.com>
2023-05-19 19:02:43 -0300

    Adiciona radios e toggle na tela de planos

--- src/app/plan/PlanForm.tsx
+++ src/app/plan/PlanForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Input } from "@/components/Input";
 import { Plan, planSchema } from "@/schemas/Plan";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -19,7 +18,7 @@ export function PlanForm() {
 
   function handleNextStep(data: Plan) {
     console.log({ data });
-    handleFormContent({ active: 2, ...data });
+    // handleFormContent({ active: 2, ...data });
   }
   return (
     <form
@@ -27,32 +26,27 @@ export function PlanForm() {
       onSubmit={handleSubmit(handleNextStep)}
     >
       <div>
-        {/* <Input
-          register={register("name")}
-          id="name"
-          title="Name"
-          type="text"
-          error={errors.name}
-          placeholder="e.g. Stephen King"
+        <label htmlFor="arcade">Arcade</label>
+        <input {...register("plan")} type="radio" value="arcade" id="arcade" />
+
+        <label htmlFor="advanced">Advanced</label>
+        <input
+          {...register("plan")}
+          type="radio"
+          value="advanced"
+          id="advanced"
         />
 
-        <Input
-          register={register("email")}
-          id="email"
-          title="Email Address"
-          type="email"
-          error={errors.email}
-          placeholder="e.g. stephenking@lorem.com"
-        />
+        <label htmlFor="pro">Pro</label>
+        <input {...register("plan")} type="radio" value="pro" id="pro" />
 
-        <Input
-          register={register("phone")}
-          id="phone"
-          title="Phone Number"
-          type="tel"
-          error={errors.phone}
-          placeholder="e.g. +1 234 567 890"
-        /> */}
+        <label className="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" value="" className="sr-only peer" />
+          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Toggle me
+          </span>
+        </label>
       </div>
 
       <button className="self-end bg-marine-blue text-light-gray py-3 px-5 rounded-lg hover:brightness-125 transition-all">
