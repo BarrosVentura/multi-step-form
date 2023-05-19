8e49aaec6d7a85e74754d2afb8baab09ae5bc8c1
Adson Ventura <adsonventura@gmail.com>
2023-05-19 14:06:20 -0300

    Adiciona estilos do form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { ListItem } from "@/components/ListItem";
-import Image from "next/image";
-// import sidebarBackground from "../../public/images/bg-sidebar-desktop.svg";
 
 export default function Home() {
   return (
-    <section className="grid grid-cols-[275px_1fr] bg-white shadow-lg p-6 rounded-xl xl:w-[80%] gap-28  min-h-[600px]">
+    <section className="grid grid-cols-[275px_1fr] bg-white shadow-lg p-6 rounded-xl xl:w-[80%] xl:max-w-[1440px] gap-28  min-h-[600px]">
       <aside
         className={`bg-[url('../../public/images/bg-sidebar-desktop.svg')] bg-no-repeat h-full rounded-lg pl-8 pt-8`}
       >
@@ -15,22 +13,47 @@ export default function Home() {
           <ListItem order={4} title="summary" />
         </ul>
       </aside>
-      <main>
-        <h1>Personal info</h1>
-        <p>Please provide your name, email address and phone number.</p>
-        <form>
-          <label htmlFor="name">Name</label>
-          <input id="name" type="text" placeholder="e.g. Stephen King" />
-          <label htmlFor="email">Email Address</label>
-          <input
-            id="email"
-            type="email"
-            placeholder="e.g. stephenking@lorem.com"
-          />
-          <label htmlFor="phone">Phone Number</label>
-          <input id="phone" type="tel" placeholder="e.g. +1 234 567 890" />
+      <main className="pt-8 flex flex-col pr-12">
+        <h1 className="text-3xl font-medium text-marine-blue mb-2">
+          Personal info
+        </h1>
+        <p className="text-cool-gray text-sm mb-4">
+          Please provide your name, email address and phone number.
+        </p>
+        <form className="flex flex-col justify-between flex-1">
+          <div>
+            <label className="text-marine-blue" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="block w-full border border-light-gray rounded-md p-2 mt-1 mb-4"
+              id="name"
+              type="text"
+              placeholder="e.g. Stephen King"
+            />
+            <label className="text-marine-blue" htmlFor="email">
+              Email Address
+            </label>
+            <input
+              className="block w-full border border-light-gray rounded-md p-2 mt-1 mb-4"
+              id="email"
+              type="email"
+              placeholder="e.g. stephenking@lorem.com"
+            />
+            <label className="text-marine-blue" htmlFor="phone">
+              Phone Number
+            </label>
+            <input
+              className="block w-full border border-light-gray rounded-md p-2 mt-1 mb-4"
+              id="phone"
+              type="tel"
+              placeholder="e.g. +1 234 567 890"
+            />
+          </div>
 
-          <button>Next Step</button>
+          <button className="self-end bg-marine-blue text-light-gray py-3 px-5 rounded-lg">
+            Next Step
+          </button>
         </form>
       </main>
     </section>
